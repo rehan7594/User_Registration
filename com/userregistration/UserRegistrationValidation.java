b9dcb0aaeb654e153f481ae96958b2cb1870d204
rehan7594 <rehansdm94@gmail.com>
2021-04-21 23:38:48 -0700

    Added: regular expression to check valid email ID

--- com/userregistration/UserRegistrationValidation.java
+++ com/userregistration/UserRegistrationValidation.java
@@ -1,10 +1,10 @@
 package com.userregistration;
 
 /*
-@description: class to checking user last name 
+@description: class to checking user email ID
 by regular expression
-@parameters: defining a method firstName() to
-check if last name is valid using regular expression 
+@parameters: defining a method email() to
+check if email is valid using regular expression 
  */
 
 import java.util.*;
@@ -12,24 +12,23 @@ import java.util.regex.*;
 
 public class UserRegistrationValidation {
 
-	//method to check user last name is valid or not by using regular expression
-	public static boolean lastName() {
+	//method to check user email is valid or not by using regular expression
+	public static boolean email() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Last Name: ");
-		String last = sc.next();
-		// Using REGEX
-		if (Pattern.matches("^[A-Z][aA-zZ]{2,}", last)) {
-			System.out.println("Last Name valid");
+		System.out.println("Enter email: ");
+		String email = sc.next();
+		if (Pattern.matches("[a-zA-Z0-9][a-zA-Z0-9_.]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+", email)) {
+
+			System.out.println("valid");
 			return true;
 		} else {
-			System.out.println("Last Name invalid");
-			return lastName(); //using recursion
+			System.out.println("invalid");
+			return email();
 		}
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration");
-		lastName();
+		email();
 	}
-
 }
