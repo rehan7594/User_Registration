81127297d9f646491b1734610f521a01f5815271
rehan7594 <rehansdm94@gmail.com>
2021-04-21 19:34:48 -0700

    Added: Welcome message

--- com/userregistration/UserRegistrationValidation.java
+++ com/userregistration/UserRegistrationValidation.java
@@ -1,5 +1,7 @@
 package com.userregistration;
 
 public class UserRegistrationValidation {
-
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration Validation");
+	}
 }
