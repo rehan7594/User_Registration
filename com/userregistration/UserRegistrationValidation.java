633b2fab57c52309eb15555e104fcb949e3705fd
rehan7594 <rehansdm94@gmail.com>
2021-04-21 22:43:22 -0700

    Added: regular expression for checking last name of username

--- com/userregistration/UserRegistrationValidation.java
+++ com/userregistration/UserRegistrationValidation.java
@@ -1,34 +1,35 @@
 package com.userregistration;
 
 /*
-	@description: class to checking user first name 
-	by regular expression
-	@parameters: defining a method firstName() to
-	check if first name is valid using regular expression 
+@description: class to checking user last name 
+by regular expression
+@parameters: defining a method firstName() to
+check if last name is valid using regular expression 
  */
 
 import java.util.*;
 import java.util.regex.*;
+
 public class UserRegistrationValidation {
 
-	//method to check user first name is valid or not by using regular expression
-	public static boolean firstName() {
+	//method to check user last name is valid or not by using regular expression
+	public static boolean lastName() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter First Name: ");
-		String first = sc.next();
+		System.out.println("Enter Last Name: ");
+		String last = sc.next();
 		// Using REGEX
-		if (Pattern.matches("^[A-Z][aA-zZ]{2,}", first)) {
-			System.out.println("First Name valid");
+		if (Pattern.matches("^[A-Z][aA-zZ]{2,}", last)) {
+			System.out.println("Last Name valid");
 			return true;
 		} else {
-			System.out.println("First Name invalid");
-			return firstName();
+			System.out.println("Last Name invalid");
+			return lastName(); //using recursion
 		}
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration");
-		firstName();
+		lastName();
 	}
-}
 
+}
