29b537dd42c5cd2e24b748d862b3bc8c01118348
rehan7594 <rehansdm94@gmail.com>
2021-04-23 19:39:04 -0700

    Added: Password rule add 1 special character

--- com/userregistration/UserRegistrationValidation.java
+++ com/userregistration/UserRegistrationValidation.java
@@ -3,14 +3,15 @@ package com.userregistration;
 
 /*
 @description: class to checking user password having 1 upper case
-and atleast 1 number by regular expression.
- 
+and atleast 1 number and 1 special character by regular expression
+@parameters: defining a method checkpassword() to
+check if password is valid using regular expression 
  */
 
 import java.util.*;
 import java.util.regex.*;
 
-public class UserRegistrationValidation{
+public class UserRegistrationValidation {
 
 	// method to check Password is valid or not by using regular expression
 	public static boolean checkPassword() {
@@ -18,7 +19,8 @@ public class UserRegistrationValidation{
 		System.out.println("Enter minimum 8 Character password : ");
 		String password = sc.next();
 
-		if (Pattern.matches("[\\S]{8,}", password) && Pattern.matches(".*[A-Z].*", password) && Pattern.matches(".*[0-9].*", password)) {
+		if (Pattern.matches("[\\S]{8,}", password) && Pattern.matches(".*[A-Z].*", password) 
+				&& Pattern.matches(".*[0-9].*", password) && Pattern.matches(".*[\\W]{1}.*", password)) {
 			System.out.println("Password valid");
 			return true;
 		} else {
