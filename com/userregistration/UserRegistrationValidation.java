2a618969559bc0ea1dc787ed7be07512484a6d8e
rehan7594 <rehansdm94@gmail.com>
2021-04-23 18:56:50 -0700

    Added: Password rules minimum 8 characters

--- com/userregistration/UserRegistrationValidation.java
+++ com/userregistration/UserRegistrationValidation.java
@@ -1,10 +1,11 @@
 package com.userregistration;
 
+
 /*
-@description: class to checking user phone number
+@description: class to checking user password
 by regular expression
-@parameters: defining a method isValidIndianMobileNumber() to
-check if phone number is valid using regular expression 
+@parameters: defining a method passwordCheck() to
+check if password is valid using regular expression 
  */
 
 import java.util.*;
@@ -12,30 +13,29 @@ import java.util.regex.*;
 
 public class UserRegistrationValidation {
 
-	//method to check valid phone number
-	public static boolean isValidIndianMobileNumber(String s) 
-	{ 
-		Pattern p = Pattern.compile("^(?:(?:\\+|0{0,2})91(\\s*[\\-]\\s*)?|[0]?)?[789]\\d{9}$"); 
+	// method to validate Password by using regular expression
+	public static boolean passwordCheck()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter minimum 8 Character password : ");
+		String password = sc.next();
 
-		Matcher m = p.matcher(s); 
-		return (m.find() && m.group().equals(s)); 
-	} 
+		// Regular Expressions
+		if (Pattern.matches("[\\S]{8,}", password)) 
+		{
+			System.out.println("Password valid");
+			return true;
+		}
+		else 
+		{
+			System.out.println("Password invalid");
+			return passwordCheck();
+		}
+	}
 
-	public static void main(String[] args) 
-	{ 
-		String phone1 = "+91-7123456789"; 
-		String phone2 = "08123456789";
-		String phone3 = "9876543210";
-		String[] phoneNumbers= {phone1,phone2,phone3};
+	public static void main(String[] args) {
+		System.out.println("Welcome to user registration");
+		passwordCheck();
+	}
 
-		for (int i = 0; i < phoneNumbers.length; i++) {
-			String phoneNumber=phoneNumbers[i];
-			if (isValidIndianMobileNumber(phoneNumber)) 
-				System.out.print(phoneNumber+" is valid mobile number"); 
-			else
-				System.out.print(phoneNumber+" is invalid mobile number"); 
-
-			System.out.println();
-		}    
-	} 
 } 
